a37e6ba3912793587f3e8724e47229909f51ad0a
Ibrahim Qandeel <iqandeel.c@stc.com.sa>
2021-06-01 18:19:03 +0300

    Change welcome message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,6 @@
 public class Main {
-
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("**************** Welcome To Code Wars Challenges Project ****************\n");
-        int x = 19;
-        int res = 19 % 10;
-		System.out.println(res);
+        System.out.println("**************** Welcome To Coding Practice Project ****************\n");
     }
 }
