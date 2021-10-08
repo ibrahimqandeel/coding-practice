35104026a9e6a3504ccbb86efe3d2487a47fd8d9
Ibrahim Qandeel <60055549+ibrahimqandeel@users.noreply.github.com>
2021-10-08 15:56:58 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.println("**************** Welcome To Code Wars Challenges Project ****************\n");
-        System.out.println("**************** Hello Hossam ****************\n");
+        System.out.println("**************** Hello Lara ****************\n");
         int x = 19;
         int res = 19 % 10;
 		System.out.println(res);
