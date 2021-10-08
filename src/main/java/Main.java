6d637db123215d6395269a961f693aabad655cd8
Ibrahim Qandeel <iqandeel.c@stc.com.sa>
2021-10-08 16:03:33 +0300

    learning git 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ public class Main {
         // TODO Auto-generated method stub
         System.out.println("**************** Welcome To Code Wars Challenges Project ****************\n");
         System.out.println("**************** Hello Lara ****************\n");
+        System.out.println("**************** Hello Ibrahim ****************\n");
         int x = 19;
         int res = 19 % 10;
 		System.out.println(res);
